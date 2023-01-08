fd88e97fc72c6ae5567006de094b3ec481342cdd
Laura Trujillo <lauratrujillo@FACTORED-JVKQC20WXG.local>
2023-01-08 16:50:35 -0500

    added layout components col sonoro

--- pages/col/col_sonoro.py
+++ pages/col/col_sonoro.py
@@ -5,13 +5,39 @@ import dash_mantine_components as dmc
 import dash_bootstrap_components as dbc
 from utils.functions import drawText, drawFigure, drawInfo
 
+header = dbc.Container([
+                    dbc.Row([
+                        dbc.Col(
+                            html.H1("Native Languages of Colombia"),
+                            style={'textAlign':'right','padding' : '0px'}),
+                        dbc.Col(drawInfo(), width=3),
+                        ], className='g-0'),
+                    ], fluid=True)
+header_metrics = dbc.Row([
+                            dbc.Col([
+                                    html.H5("Colombia Metrics"),
+                                    html.Hr(),
+                            ],  style={'textAlign': 'center'}),
+                        ])
+metrics = dbc.Row([
+                    dbc.Col([
+                            drawText(),
+                            ], width=4),
+                    dbc.Col([
+                            drawText(),
+                            ], width=4),
+                    dbc.Col([
+                            drawText(),
+                            ], width=4),
+                ])
+table = dbc.Row([
+                            dbc.Col([
+                               drawFigure(id_='sonoro-table')
+                            ]),
+                        ])
 
 layout = html.Div([
-    dbc.Container([
-        dbc.Row([
-            dbc.Col(html.H1("Native Languages of Colombia"),style={'textAlign':'right','padding' : '0px'}),
-            dbc.Col(drawInfo(), width=3),
-        ], className='g-0'),
+    header,
     html.Hr(),
     dbc.Card([
         dbc.CardBody([
@@ -20,32 +46,12 @@ layout = html.Div([
                     drawFigure(id_='sonoro-graph')
                 ], md=7),
                 dbc.Col([
-                        dbc.Row([
-                            dbc.Col([
-                                    html.H5("Colombia Metrics"),
-                                    html.Hr(),
-                            ],  style={'textAlign': 'center'}),
-                        ]),
-                        dbc.Row([
-                            dbc.Col([
-                               drawText(),
-                            ], width=4),
-                            dbc.Col([
-                               drawText(),
-                            ], width=4),
-                            dbc.Col([
-                               drawText(),
-                            ], width=4),
-                        ]),
+                        header_metrics,
+                        metrics,
                         html.Br(),
-                        dbc.Row([
-                            dbc.Col([
-                               drawFigure(id_='sonoro-table')
-                            ]),
-                        ])
+                        table
                 ], md=5)
-            ])
-        ])
+            ]),
+        ]),
     ], body=True)
-    ], fluid=True),
 ])
