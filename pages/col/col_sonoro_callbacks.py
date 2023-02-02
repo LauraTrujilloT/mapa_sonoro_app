68dbea6a893686cda2d05bceb8761272f91ef056
Laura Trujillo <lauratrujillo@FACTORED-JVKQC20WXG.local>
2023-02-02 14:12:05 -0500

    added dane census disclaimer

--- pages/col/col_sonoro_callbacks.py
+++ pages/col/col_sonoro_callbacks.py
@@ -246,7 +246,8 @@ def update_indicators(family, depto):
         value=col_df.n_hablantes.sum(),    
         domain={'x': [0., 0.33], 'y': [0.0, 1.]},  
         number={'valueformat':',r', 'font':{'size':35}},  
-        title={'text': "Total Speakers", 'font':{'size':15}})
+        title={'text': "Total Speakers", 'font':{'size':15}},
+        )
     trace2 = go.Indicator(
         mode="number",    
         value=col_df.nombre_lengua.nunique(),    
@@ -262,12 +263,22 @@ def update_indicators(family, depto):
 
     # layout and figure production
     layout = go.Layout(
-                    #paper_bgcolor='rgb(0,0,0)',
+                    plot_bgcolor='rgba(0,0,0,0)',
+                    xaxis={'showgrid': False, 'showticklabels':False, 'range':[-1,1]},
+                    yaxis={'showgrid': False, 'showticklabels':False, 'range':[0,1]},
                     margin=dict(l=20, r=20, t=20, b=20),
                     autosize=True,
                     #height=100
                     )
     fig = go.Figure(data = [trace1, trace2, trace3], layout = layout)
+    fig.add_annotation(
+                text='out of <b>48,258,494</b> colombians <I>(DANE, 2018 Census)</I>',
+                x=0.2,
+                y=0.1,
+                xref='x',
+                yref='y',
+                showarrow=False,
+    )
 
     return fig
 
